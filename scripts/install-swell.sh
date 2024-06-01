517a44a8a5b4125ec89748846bf85e157cf2eaaf
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-06-01 10:38:13 -0400

    Refactor GitHub actions into standalone scripts

---
+++ scripts/install-swell.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Make experiment directory
+mkdir /discover/nobackup/gmao_ci/swell/tier1/${GITHUB_RUN_ID}
+
+# Copy and source modules
+cp ${GITHUB_WORKSPACE}/src/swell/deployment/platforms/nccs_discover/modules /discover/nobackup/gmao_ci/swell/tier1/${GITHUB_RUN_ID}/
+source /discover/nobackup/gmao_ci/swell/tier1/${GITHUB_RUN_ID}/modules
+pip install --prefix=/discover/nobackup/gmao_ci/swell/tier1/${GITHUB_RUN_ID}/swell -r ${GITHUB_WORKSPACE}/requirements.txt --no-cache-dir ${GITHUB_WORKSPACE}
+
+# Remove source code (needed to ensure nothing relies on the source)
