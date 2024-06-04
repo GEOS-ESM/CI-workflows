dd2f6114f7893482569f953e2a6f6dbab4069e1d
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-06-04 13:47:47 -0400

    Bugfix export statements

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -3,13 +3,13 @@
 github_run_id() {
   # Set default value to `LOCAL_$(uuidgen) if unset`
   : "${GITHUB_RUN_ID:=LOCAL_$RANDOM}"
-  export $GITHUB_RUN_ID
+  export GITHUB_RUN_ID
 }
 
 github_workspace() {
   # Set default value to current working directory
   : "${GITHUB_WORKSPACE:=$PWD}"
-  export $GITHUB_WORKSPACE
+  export GITHUB_WORKSPACE
 }
 
 github_variables() {
